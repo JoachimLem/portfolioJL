acd1aa8e035d9ba5e59816b202b06361af1dc34a
JoachimLem <joalemweb@gmail.com>
2022-01-31 12:10:58 +0100

    add description on homePage

--- src/Components/HomePage/index.js
+++ src/Components/HomePage/index.js
@@ -18,18 +18,25 @@ const HomePage = () => {
         </div>
         <p className="logo-text">Développeur Web/Mobile</p>
       </div>
-
+      {/* ******************************************* */}
 
       {/* Title */}
       <div className="title">
-      <h1 className="main-title">Développeur Front-End</h1>
-      <h2 className="secondary-title">Bienvenue sur mon Portfolio</h2>
+        <h1 className="main-title">Développeur Front-End</h1>
+        <h2 className="secondary-title">Bienvenue sur mon Portfolio</h2>
       </div>
+      {/* ******************************************* */}
 
 
       {/* Description to portfolio */}
-      <p>Je suis la homePage</p>
+      <p className="description">Découvrez mes réalisations, qui je suis.
+Je réalise des applications web , site e-commerce
+ou bien encore site vitrine.
+Si vous souhaitez gagner en visibilité dans le monde digital, je peux vous aider à y parvenir.</p>
+      {/* ******************************************* */}
+
     </div>
+    
   )
 
 };
