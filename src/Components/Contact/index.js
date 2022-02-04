3a1088b75c8d3b48c9601d6228c1f177f9e0dd0d
JoachimLem <joalemweb@gmail.com>
2022-02-04 12:05:34 +0100

    add form into contact section

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import './contact.css';
+import { useForm } from "react-hook-form";
 
-const Contact = ({id}) => (
-  <div className='container' id={id}>Contact</div>
-);
+const Contact = ({id}) => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(errors);
+  
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="Prénom" {...register("Prénom", {required: true, maxLength: 80})} />
+      <input type="text" placeholder="Nom" {...register("Nom", {required: true, maxLength: 100})} />
+      <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
+      <select {...register("Type de demande", { required: true })}>
+        <option value="Offre d'emploi">Offre d'emploi</option>
+        <option value="Signaler un bug">Signaler un bug</option>
+        <option value="Commentaires">Commentaires</option>
+        <option value="Autres">Autres</option>
+      </select>
+      <textarea {...register("Message", {required: true, maxLength: 250})} />
+
+      <input type="submit" />
+    </form>
+  );
+  
+};
 
 export default Contact;
+
