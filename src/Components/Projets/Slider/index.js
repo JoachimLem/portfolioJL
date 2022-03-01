2fe31509f584d4bf7756347e2212a2ef2a6a65b3
JoachimLem <joalemweb@gmail.com>
2022-03-01 16:38:34 +0100

    bug minors fixed

--- src/Components/Projets/Slider/index.js
+++ src/Components/Projets/Slider/index.js
@@ -7,25 +7,17 @@ const cardsProjects = [
 
   {
     id: 1,
-    title: 'Video Chat',
-    description: 'Reprehenderit adipisicing minim sit non aliquip voluptate labore sunt. Aliquip amet eiusmod non exercitation reprehenderit magna veniam culpa culpa laboris quis laboris cillum sint. Cillum pariatur quis nisi id quis ullamco excepteur. Magna officia dolore enim consequat anim cupidatat cupidatat minim minim tempor qui. Occaecat aute est anim laborum. Quis adipisicing cupidatat minim proident cupidatat voluptate ex sunt id aute do nisi ex eu. Mollit laboris incididunt veniam qui cupidatat sunt velit.',
-    image: '/assets/images/youtubeIllustation.png',
+    title: 'EN COURS',
+    description: `Plusieurs projets, sont en cours d'élaboration et dès que l'un d'eux sera terminé vous pourrez y avoir accès via l'intermédiaire du bouton "Découvrir".
+
+    Plusieurs technologie seront utilisés et un panel très large d'application seront proposé pour couvrir un large panel de mes compétences.
+    
+    Je vous donne rendez-vous très bientôt et si vous avez des idées, suggestions rendez-vous dans la partie Contact et envoyé moi un message.`,
+    image: '/assets/images/workInProgress.jpg',
     link: 'www.google.com'
   },
-  {
-    id: 2,
-    title: 'Football HightLights',
-    description: 'ReEnim exercitation minim do ut culpa occaecat. Officia reprehenderit duis culpa sint duis irure nulla consequat esse dolore. Pariatur anim ut est cupidatat officia ipsum est laborum irure dolore ullamco aliquip aliqua consectetur. Duis fugiat elit dolore aliqua cillum aute dolor duis pariatur aute laboris cupidatat ullamco ipsum. Veniam commodo qui voluptate duis nisi culpa enim exercitation. Fugiat reprehenderit nulla aute laboris.',
-    image: '/assets/images/hightLightsFootball.jpeg',
-    link: 'www.staderennais.com'
-  },
-  {
-    id: 3,
-    title: 'Crêpes Party',
-    description: 'Duis anim anim laboris esse. Elit do consectetur magna aute fugiat amet sunt mollit. Commodo sunt nulla in sit aliqua dolor consequat culpa sit nulla commodo. Ullamco voluptate cupidatat anim adipisicing deserunt sit reprehenderit anim et incididunt laboris. Duis deserunt ut tempor Lorem officia adipisicing excepteur labore Lorem dolore.',
-    image: '/assets/images/crepes.jpeg',
-    link: 'www.bretagne.com'
-  },
+ 
+
 ];
 
 export default function Slider() {
